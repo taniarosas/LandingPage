a0a7e7b23da34475e23c24ed83667d3859688519
tania rosas-jimenez <rosastania@yahoo.com>
2015-05-23 20:51:56 -0700

    added links and stylized

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="main.css">
+	<link href='http://fonts.googleapis.com/css?family=Allerta+Stencil' rel='stylesheet' type='text/css'>
 	<title></title>
 </head>
 <body>
@@ -13,12 +14,32 @@
 	<hr>
 		<span>
 	</hr>	
-			Los Angeles, California
+			
+			<A id="la" HREF = "contact.php">Contact</A>
+			<A id="la" HREF = "projects.php">Projects</A>
 	<hr>		
 		</span>
 	</hr>
 </div>
-<div class="circular"></div>
+<div class="circular">	
+</div>
+<div id="bar2">
+</div>
+<footer id="footer">
+	<div class="soc">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<ul class="socials">
+ 
+  <li id="f"><a href="http://facebook.com"><i class="fa fa-facebook"></a></i></li>
+  <li><a href="http://mail.google.com"><i class="fa fa-google"></a></i></li>
+  <li id="t"><a href="http://twitter.com"><i class="fa fa-twitter"></a></i></li>
+  <li><a href="http://youtube.com"><i class="fa fa-youtube"></a></i></li>
+<!--  add anchor tags, and off u go -->
+  
+    </ul> 
+</div>
+
+</footer>
 </div>
 </body>
 </html>
--- projects.php
+++ projects.php
@@ -2,10 +2,43 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="main.css">
+	<link href='http://fonts.googleapis.com/css?family=Allerta+Stencil' rel='stylesheet' type='text/css'>
 	<title></title>
 </head>
 <body id="projects">
+<div id="bar1">	
+<div id="letter">
+	T
+</div>
+<div id="la">
+	<hr>
+		<span>
+	</hr>	
+			
+			<A id="la" HREF = "index.php">Home</A>
+			<A id="la" HREF = "contact.php">Contact</A>
+	<hr>		
+		</span>
+	</hr>
+</div>
+<div id="bar2">
+</div>
+<footer id="footer">
+	<div class="soc">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<ul class="socials">
+ 
+  <li id="f"><a href="http://facebook.com"><i class="fa fa-facebook"></a></i></li>
+  <li><a href="http://mail.google.com"><i class="fa fa-google"></a></i></li>
+  <li id="t"><a href="http://twitter.com"><i class="fa fa-twitter"></a></i></li>
+  <li><a href="http://youtube.com"><i class="fa fa-youtube"></a></i></li>
+<!--  add anchor tags, and off u go -->
+  
+    </ul> 
+</div>
 
+</footer>
+</div>
 
 </body>
 </html>
